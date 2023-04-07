a6556d1061a5f66ce999a60d04401045956555a2
Pedro Gabriel Prazeres <pedrogprazeres@gmail.com>
2023-04-07 17:45:47 -0300

    remove views

--- recipes/views.py
+++ recipes/views.py
@@ -4,9 +4,5 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'recipes/home.html')
 
-def contato(request):
-    return render(request, 'recipes/contato.html')
 
-def sobre(request):
-    return HttpResponse('SOBRE')
 
